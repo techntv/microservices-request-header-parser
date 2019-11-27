cc261d1db1726864b04e27a7b2cc38832772ff76
The Vinh <vinh.nguyenthe@haravan.com>
2019-11-27 23:02:07 +0700

    init commit

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/whoami", function (req, res) {
+  let ipaddress = req.header('X-Forwarded-For').split(',')[0];
+  let language = req.headers['accept-language'];
+  let software = req.headers['user-agent']
+  res.json({
+    ipaddress,
+    language,
+    software
+  });
+});
+
+app.listen(4000, function () {
+  console.log('Your app is listening on port ' + 4000);
+});
